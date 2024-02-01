cf2a3f65f2b00a81cd555e76a4303b5b6bcc61e5
= <=>
2024-02-01 13:22:40 +0100

    Remove unused files and dependencies

---
+++ next/pages/Home.jsx
@@ -0,0 +1,14 @@
+// pages/Home.jsx
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Contenido de la página principal */}
+    </div>
+  );
+};
+
+export default Home;
