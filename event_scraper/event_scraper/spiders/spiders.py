94bfe1ecbef3d519d5b82e3d229f5a043c836877
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-02-06 11:12:14 +0100

    Refactor spider and delete unused file

--- event_scraper/event_scraper/spiders/spiders.py
+++ event_scraper/event_scraper/spiders/spiders.py
@@ -1,12 +1,43 @@
 import scrapy
+import re
 
-class MiSpider(scrapy.Spider):
-    name = 'mi_spider'
-    start_urls = ['https://www.ticketmaster.es/musica/todos-musica/10001/events?cities=158715']  # Cambia esta URL por la que desees hacer scraping
+
+class ConciertosSpider(scrapy.Spider):
+    name = "conciertos"
+    start_urls = [
+        "https://www.elcorteingles.es/entradas/lo-mas-vendido/barcelona/?filters%5Bgenre%5D=conciertos"
+    ]
+
+    def start_requests(self):
+        self.logger.info("Iniciando el Spider...")
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse, errback=self.error_handler)
 
     def parse(self, response):
-        # Extraer los elementos <h1>
-        for h1_tag in response.css('h1'):
-            yield {
-                'h1_text': h1_tag.get()  # Obtener el texto dentro de <h1>
-            }
+        self.logger.info("Página accedida correctamente: %s", response.url)
+        # for evento_link in response.css(".product-card a::attr(href)").getall():
+        #     yield scrapy.Request(
+        #         response.urljoin(evento_link), callback=self.parse_evento
+        #     )
+
+    # def parse_evento(self, response):
+    #     titulo_raw = response.css(".product-header__main-title::text").get()
+    #     patron_localizacion = r"\ben [A-Za-z]+(?: [A-Za-z]+)?\b"
+    #     titulo_sin_concierto = titulo_raw.replace("Concierto ", "")
+    #     titulo = re.sub(patron_localizacion, "", titulo_sin_concierto)
+
+    #     fecha = response.css(".h5.product-header_text::text").get()
+    #     recinto = response.css(
+    #         ".product-header__bottom__item-link.link--underline.margintop0::text"
+    #     ).get()
+    #     lugar = response.css(".lugar::text").get()
+
+    #     yield {
+    #         "titulo": titulo,
+    #         "fecha": fecha,
+    #         "recinto": recinto,
+    #         "lugar": lugar,
+    #     }
+
+    def error_handler(self, failure):
+        self.logger.error("Error al acceder a la página: %s", failure.request.url)
