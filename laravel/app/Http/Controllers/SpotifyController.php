87b35c2159c3e6babcd97c6ec29e448078f420b1
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-02-08 12:16:23 +0100

    Add Spotify and Ticketmaster API credentials and update routes

--- laravel/app/Http/Controllers/SpotifyController.php
+++ laravel/app/Http/Controllers/SpotifyController.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\URL;
 class SpotifyController extends Controller
 {
     public function login()  {
-        $client_id = '5467f1a23dd643079df61dee264117f3';
-        $redirectUri = 'http://localhost:8000/api/callback';
+        $client_id = env('SPOTIFY_CLIENT_ID');
+        $redirectUri = env('SPOTIFY_REDIRECT_URI');
         $state = $this->generateRandomString(16);
         $scope = 'user-read-email user-library-read user-top-read';
 
