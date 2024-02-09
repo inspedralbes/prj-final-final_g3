61fc95353acbb0afd64e76d6e3349a99d0506a83
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-02-09 12:50:52 +0100

    Update docker-compose.yml and package.json

--- node/server.js
+++ node/server.js
@@ -1,11 +1,1 @@
-import express from 'express';
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('¡Hola Mundo!');
-});
-
-app.listen(port, () => {
-    console.log(`Servidor Express escuchando en el puerto ${port}`);
-});
+console.log('Hello, world!!!');
