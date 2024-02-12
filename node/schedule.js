f4fd969b0467087149e7a763bf8f2aa7f58997f4
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-02-12 18:47:09 +0100

    Add scheduled task to store events

--- node/schedule.js
+++ node/schedule.js
@@ -1,11 +1,13 @@
 import cron from 'node-cron';
 import server from './server.js';
 
+server.storeEvents();
 
+// Ejecuta el método storeEvents() a las 00:00 y 12:00 todos los días
 cron.schedule('0 0 0,12 * * *', () => {
     console.log('Running cron job');
     server.storeEvents();
 }, {
     scheduled: true,
-    timezone: "Europe/Madrid", // Configura la zona horaria aquí
+    timezone: "Europe/Madrid",
 });
