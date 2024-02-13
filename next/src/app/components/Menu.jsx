32a8bccd613097e938f23fd8295f7c9108f441b1
= <=>
2024-02-13 13:15:11 +0100

    Update Menu component styling

--- next/src/app/components/Menu.jsx
+++ next/src/app/components/Menu.jsx
@@ -9,24 +9,24 @@ import User from './Icons/User'
 const Menu = () => {
     const isLogged = false;
     return (
-        <section className='bg-gradient-to-r from-orange-500 to-yellow-400 w-screen sticky bottom-0 py-1 text-black flex justify-around items-center'>
+        <section className=' w-screen sticky bottom-0 py-1 text-white flex justify-around items-center backdrop-blur-lg'>
             <Link href="/" className='flex flex-col justify-center items-center '>
-                <Home size="32" />
+                <Home size="28" />
                 <p className='font-bold text-xs'>Inicio</p>
             </Link>
 
             <Link href="/events" className='flex flex-col justify-center items-center '>
-                <Speaker size="32" />
+                <Speaker size="28" />
                 <p className='font-bold text-xs'>Eventos</p>
             </Link>
 
             <Link href={isLogged ? "/chat" : "/join"} className='flex flex-col justify-center items-center '>
-                <Chat size="32" />
+                <Chat size="28" />
                 <p className='font-bold text-xs'>Chat</p>
             </Link>
 
             <Link href={isLogged ? "/perfil" : "/join"} className='flex flex-col justify-center items-center '>
-                <User size="32" />
+                <User size="28" />
                 <p className='font-bold text-xs'>Perfil</p>
             </Link>
         </section>
