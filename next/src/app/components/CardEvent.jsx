696b5b044f8c524601d4104f99145453dc19d794
a22ivbs <a22ivabarsan@inspedralbes.cat>
2024-02-14 12:23:40 +0100

    Fix SVG class names in login and register pages

--- next/src/app/components/CardEvent.jsx
+++ next/src/app/components/CardEvent.jsx
@@ -10,7 +10,7 @@ import Users from './Icons/Users';
 import Heart from './Icons/Heart';
 import HeartFill from './Icons/HeartFill';
 
-const CardEvent = ({ image, name, location, date, people }) => {    
+const CardEvent = ({ image, name, location, date, people }) => {
     const [liked, setLiked] = useState(false);
 
     const toggleLike = () => {
@@ -21,20 +21,22 @@ const CardEvent = ({ image, name, location, date, people }) => {
     const isLogged = Loged.isLoged;
 
     return (
-        <div className='relative w-full h-56 rounded-xl overflow-hidden'>
-            <img className='w-full h-full brightness-[.4] object-cover' src={image} alt="" />
-            <div className='absolute bottom-2 left-2'>
-                <h2 className='text-white text-xl font-semibold text-left'>{name}</h2>
-                <p className='text-white flex items-center gap-1'><MapPin size="18" />{location}</p>
-                <p className='text-white flex items-center gap-1'><Calendar size="18" />{date}</p>
-                <p className='text-white flex items-center gap-1'><Users size="18" />{people} personas</p>
+        <Link href={isLogged ? '#' : '/join'}>
+            <div className='relative w-full h-56 rounded-xl overflow-hidden'>
+                <img className='w-full h-full brightness-[.4] object-cover' src={image} alt="" />
+                <div className='absolute bottom-2 left-2'>
+                    <h2 className='text-white text-xl font-semibold text-left'>{name}</h2>
+                    <p className='text-white flex items-center gap-1'><MapPin size="18" />{location}</p>
+                    <p className='text-white flex items-center gap-1'><Calendar size="18" />{date}</p>
+                    <p className='text-white flex items-center gap-1'><Users size="18" />{people} personas</p>
+                </div>
+                <button
+                    className={`absolute bottom-2 right-2 p-1 rounded-lg ${liked ? 'bg-red-500 hover:bg-red-700' : 'bg-green-500 hover:bg-green-700'}`}
+                    onClick={toggleLike}
+                >{liked ? <HeartFill size="20" /> : <Heart size="20" />}
+                </button>
             </div>
-            <button
-                className={`absolute bottom-2 right-2 p-1 rounded-lg ${liked ? 'bg-red-500 hover:bg-red-700' : 'bg-green-500 hover:bg-green-700'}`}
-                onClick={toggleLike}
-            >{liked ? <HeartFill size="20" /> : <Heart size="20" />}
-            </button>
-        </div>
+        </Link>
     );
 };
 
--- next/src/app/events/page.jsx
+++ next/src/app/events/page.jsx
@@ -4,18 +4,16 @@ import React, { useState, useEffect, useDispatch } from 'react';
 import axios from 'axios';
 import CardEvent from '../components/CardEvent';
 import Menu from '../components/Menu';
-import Link from 'next/link';
 
 const Page = () => {
-  const isLogged = false;
   const [eventos, setEventos] = useState([]);
-  
-   useState(() => {
+
+  useState(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:8000/api/events');
         // console.log(await response);
-        
+
         const eventos = response.data.events;
         const eventosAgrupados = {};
         eventos.forEach((evento) => {
@@ -24,7 +22,7 @@ const Page = () => {
             eventosAgrupados[key] = evento; // Si no existe un evento con esa clave o si el nombre del evento actual es más corto, actualizarlo
           }
         });
-        
+
         console.log(await eventos);
         setEventos(Object.values(eventosAgrupados)); // Convertir el objeto en un array
       } catch (error) {
@@ -39,21 +37,19 @@ const Page = () => {
       <main className='w-[90vw] min-h-screen mx-auto py-4 flex flex-col gap-6 relative'>
         <h1 className='text-center uppercase text-2xl font-bold'>Los proximos eventos mas top</h1>
 
-        <Link href={isLogged ? '#' : '/join'}>
-          <section className='flex flex-col gap-3'>
-            {eventos.map((evento, index) => (
-              <CardEvent
-                key={index}
-                image={JSON.parse(evento.images)[2]}
-                name={evento.event}
-                location={evento.city}
-                date={evento.date}
-                people={evento.asistentes}
-              />
+        <section className='flex flex-col gap-3'>
+          {eventos.map((evento, index) => (
+            <CardEvent
+              key={index}
+              image={JSON.parse(evento.images)[2]}
+              name={evento.event}
+              location={evento.city}
+              date={evento.date}
+              people={evento.asistentes}
+            />
 
-            ))}
-          </section>
-        </Link>
+          ))}
+        </section>
       </main>
       <Menu />
     </>
