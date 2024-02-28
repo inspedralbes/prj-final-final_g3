992ec7c9f4145023a1282cd5f8862ba3eeed72e1
= <=>
2024-02-28 11:52:56 +0100

    Add user login functionality to Header component

--- next/src/app/components/Header.jsx
+++ next/src/app/components/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Link from 'next/link';
+'use client'
+
+import React, { useContext } from 'react'
+import Link from 'next/link'
+import { UserLoged } from '../context/UserLoged'
+
 
 const Header = () => {
+  const Loged = useContext(UserLoged);
+  const isLogged = Loged.isLoged;
+
   return (
     <header className='w-[90%] py-4 mx-auto flex justify-between'>
       <h1 className='text-2xl font-bold text-white uppercase'>Spottunes</h1>
-      <Link href='/login'>Inicia sessió</Link>
+      {!isLogged && <Link href='/login'>Inicia sessió</Link>}
     </header>
   )
 }
