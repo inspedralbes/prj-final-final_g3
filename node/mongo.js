151e4059dc55a6affcc3466cc95c43c1ed6ea4cb
Pedro Garcia <a20pedgarguz@inspedralbes.cat>
2024-04-19 13:16:59 +0200

    Update express version in package.json and mongo.js

--- node/mongo.js
+++ node/mongo.js
@@ -1,37 +1,37 @@
 import mongoose from "mongoose";
 import models from "./models.js";
 import minimist from "minimist";
+import express from 'express';
+
+const app = express();
+
 
 const argv = minimist(process.argv.slice(2));
 const host = argv.host || 'mongodb';
 
 mongoose.connect('mongodb://root:root@' + host + ':27017/spottunes', { authSource: "admin" })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-const posts = [
-    {
-        content: "Hello World!",
-        likes: 0,
-        comments: 0,
-    },
-    {
-        content: "This is a post",
-        likes: 0,
-        comments: 0,
-    },
-    {
-        content: "This is another post",
-        likes: 0,
-        comments: 0,
-    },
-];
+    const posts = [
+        {
+            content: "Hello World!",
+            likes: [],
+            comments: 0,
+        },
+        {
+            content: "This is a post",
+            likes: [],
+            comments: 0,
+        },
+        {
+            content: "This is another post",
+            likes: [],
+            comments: 0,
+        },
+    ];
 
 for (const post of posts) {
     models.post.create(post);
     console.log("Post created");
 }
-
-
-
-
