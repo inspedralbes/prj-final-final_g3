0daac9aee7b8ff30be55819bd64488d0a3fd9661
a16margarlop <a16margarlop@inspedralbes.cat>
2024-04-30 12:31:34 +0200

    nuxt setup

---
+++ nuxt/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class='w-[90%] py-4 mx-auto flex justify-between'>
+      <h1 class='text-2xl font-bold text-white uppercase'>Spottunes</h1>
+      <p class="text-white">Inicia sesio</p>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
