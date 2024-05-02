ffb5b2e3e357923495b8a959a00954f0c9af0f93
a22ivbs <a22ivabarsan@inspedralbes.cat>
2024-05-02 13:09:04 +0200

    to nuxt all

---
+++ nuxt/components/Icons/Heart.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width=size :height=size viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
+</template>
+
+<script>
+    export default {
+        props: {
+            size: {
+                type: String,
+                default: '24'
+            },
+        },
+        mounted() {
+            console.log("Size:",this.size);
+        },
+        methods: {
+        }
+    }
+</script>
