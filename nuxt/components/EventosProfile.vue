70cd66333913bf3aa62e76c85df0128e301d493d
a16margarlop <a16margarlop@inspedralbes.cat>
2024-05-03 08:43:22 +0200

    make nuxt components and pages

---
+++ nuxt/components/EventosProfile.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class='flex justify-center text-white'>
+        <p>No tienes eventos disponibles.</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
---
+++ nuxt/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EEC227",
+        background: "#212121",
+      },
+    },
+  },
+  plugins: [],
+}
+
