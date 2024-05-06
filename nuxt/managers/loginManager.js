acd415c2a048072dde439c675f662210dc337db9
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-06 15:37:07 +0200

    Created loginManager

---
+++ nuxt/managers/loginManager.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+let env = import.meta.env.VITE_APP_ENV;
+let url;
+
+if (env.toLowerCase() === "development") {
+  url = import.meta.env.VITE_APP_API_DEV_URL;
+} else if (env.toLowerCase() === "production") {
+  url = import.meta.env.VITE_APP_API_PROD_URL;
+}
