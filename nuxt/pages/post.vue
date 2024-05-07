0822f064800e7a8794927e3c4e0646d873b8957d
DGrazz <mariogalo2008@gmail.com>
2024-05-07 13:23:00 +0200

    Update post.vue component with new layout and functionality

--- nuxt/pages/post.vue
+++ nuxt/pages/post.vue
@@ -1,12 +1,31 @@
 <template>
-    <div class="text-white text-center">
-        Aqui se podra subir un post
-    </div>
-    <Menu/>
+    <section class="text-white">
+        <header class="w-[90%] h-[7vh] mx-auto flex justify-between items-center">
+            <NuxtLink to="/events">Cancelar</NuxtLink>
+            <button
+                class="bg-primary rounded-full px-4 py-1 font-semibold text-sm hover:bg-primary/80 transition duration-200">Post</button>
+        </header>
+
+        <main class="bg-black h-[93vh] flex justify-between items-start px-[5%] pt-4">
+            <div class="w-full flex justify-between items-center">
+                <img class="size-14 rounded-full object-cover" src="https://i.ytimg.com/vi/LuWV8Wh00Nk/maxresdefault.jpg"
+                alt="">
+                <textarea class="w-full bg-transparent outline-none px-2 border" rows="1" placeholder="Escribe aqui..."></textarea>
+            <button class="rounded-full p-1 bg-[#818181]">
+                <AddImage class="size-5" />
+            </button>
+            </div>
+        </main>
+    </section>
 </template>
 
 <script>
-    export default {
-        
+import AddImage from '../components/Icons/AddImage.vue'
+export default {
+    components: {
+        AddImage,
     }
+
+    
+}
 </script>
