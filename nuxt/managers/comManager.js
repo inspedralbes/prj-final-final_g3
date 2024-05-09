ace9fff58051039dafac75ca91f784136f6dfb85
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-09 16:06:50 +0200

    chore: Refactor toggleLike method and update API URLs in comManager.js

--- nuxt/managers/comManager.js
+++ nuxt/managers/comManager.js
@@ -9,7 +9,7 @@ if (env.toLowerCase() === "development") {
   url_api = import.meta.env.VITE_APP_API_DEV_URL;
   url_api_mongo = import.meta.env.VITE_APP_MONGO_API_DEV_URL;
 } else if (env.toLowerCase() === "production") {
-  url = import.meta.env.VITE_APP_API_PROD_URL;
+  url_api = import.meta.env.VITE_APP_API_PROD_URL;
   url_api_mongo = import.meta.env.VITE_APP_API_PROD_MONGO_URL;
 }
 
@@ -42,28 +42,6 @@ async function getEvents() {
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-
-  // try {
-  // const response = await axios.get('http://localhost:8000/api/events');
-  // const eventos = response.data.events;
-  // const eventosAgrupados = {};
-  // eventos.forEach((evento) => {
-  // const key = `${evento.artist}-${evento.date}`;
-  // if (!eventosAgrupados[key] || evento.event.length < eventosAgrupados[key].event.length) {
-  //   eventosAgrupados[key] = evento;
-  // }
-  // try {
-  //       const response = await axios.get(`http://localhost:8080/likeEvents?userId=${User.id}`);
-  //       setEventosLike(response.data);
-  // } catch (error) {
-  //       console.error('Error fetching data:', error);
-  // }
-  // });
-  // this.eventos = Object.values(eventosAgrupados);
-  // console.log(this.eventos);
-  // } catch (error) {
-  // console.error('Error fetching data:', error);
-  // }
 }
 
 async function getLikeEvents() {
@@ -80,8 +58,38 @@ async function getLikeEvents() {
   }
 }
 
+async function likeAnEvent(eventID) {
+  const store = useStores();
+  const User = store.getUserInfo();
+  try {
+    const response = await axios.post(`${url_api_mongo}/likeEvent`, {
+      userId: User.id,
+      eventId: eventID,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+async function unlikeAnEvent(eventID) {
+  const store = useStores();
+  const User = store.getUserInfo();
+  try {
+    const response = await axios.delete(`${url_api_mongo}/likeEvent`, {
+      userId: User.id,
+      eventId: eventID,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
 const comManager = {
   getEvents,
+  likeAnEvent,
+  unlikeAnEvent,
 };
 
 export default comManager;
