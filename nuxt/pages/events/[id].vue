f0fd2d29b729f31ba3b355a190bc8f962019ffe9
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-09 15:30:03 +0200

    Refactor event page layout and add lazy loading for event images

--- nuxt/pages/events/[id].vue
+++ nuxt/pages/events/[id].vue
@@ -1,24 +1,24 @@
 <template>
   <main class="min-h-screen relative">
     <header class='w-[80%] flex justify-between absolute top-4 left-1/2 -translate-x-1/2 z-10'>
-      <!-- <NuxtLink to='/events'>
-        <IconsLeftArrow class="size-6" />
-      </NuxtLink> -->
+      <NuxtLink to='/events'>
+        <IconsLeftArrow class="size- bg-black rounded-full bg-opacity-60" />
+      </NuxtLink>
 
-      <!-- <button @click="toggleLike" :class="{
+      <button @click="toggleLike" :class="{
         'px-4 py-1 text-black font-semibold rounded-full text-sm transition duration-300': true,
         'bg-red-400 hover:bg-red-600 text-white': liked,
         'bg-white hover:bg-green-500 hover:text-white': !liked
       }">
-      {{ liked ? 'Siguiendo' : 'Seguir' }}
-    </button> -->
+        {{ liked ? 'Siguiendo' : 'Seguir' }}
+      </button>
     </header>
     <div class='h-[218px] relative'>
-      <!-- <img :src="JSON.parse(this.event.images)[2]" class='w-full h-full object-cover' alt="foto" /> -->
-      <!-- <div class='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-50% from-transparent to-[#212121]'></div> -->
+      <img v-if="eventImage" :src="eventImage" class='w-full h-full object-cover' :alt="`Foto de ${event.event}`" />
+      <div class='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-50% from-transparent to-[#212121]'></div>
     </div>
 
-    <!-- <article class='w-[80%] mx-auto flex flex-col gap-3 mt-6'>
+    <article class='w-[80%] mx-auto flex flex-col gap-3 mt-6'>
       <p v-if="event.genre && event.genre !== 'Undefined'" class="text-xs text-[#A4A4A4]">{{ event.genre }}</p>
       <h1 class='text-2xl font-bold'>{{ event.event }}</h1>
       <div class='flex justify-between items-center'>
@@ -39,9 +39,9 @@
           </h2>
         </div>
       </div>
-      <h3 class='text[#CACACA] text-sm'>2.487 personas inscritas</h3>
+      <!-- <h3 class='text[#CACACA] text-sm'>2.487 personas inscritas</h3> -->
       <div class='w-full h-[2px] bg-[#888888]'></div>
-    </article> -->
+    </article>
   </main>
 </template>
 
@@ -55,28 +55,31 @@ export default {
       liked: false,
       eventID: parseInt(this.$route.params.id),
       event: {},
+      eventImage: ''
     }
   },
   created() {
-    console.log(this.$route.params.id);
+    this.getEventById();
   },
   mounted() {
-    console.log(this.store.events);
-    this.getEventById();
+
   },
   methods: {
     getEventById() {
       this.event = this.store.events.find(event => event.id === this.eventID);
-      console.log(this.event);
     },
+    toggleLike() {
+      this.liked = !this.liked;
+    }
   },
   computed: {
-    // getEventImage() {
-    //   return JSON.parse(this.event.images)[2];
-    // }
+
   },
   watch: {
-
+    event() {
+      const imagesJSON = JSON.parse(this.event.images);
+      this.eventImage = imagesJSON[2];
+    }
   }
 }
 </script>
