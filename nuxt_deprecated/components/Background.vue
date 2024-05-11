2e1aad2044a8f68dbc09d6c1d462833f147df601
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-11 13:35:30 +0200

    Refactor and remove deprecated Nuxt files and components

--- nuxt_deprecated/components/Background.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <img class='absolute -z-10 w-screen h-screen object-cover brightness-50' src="/public/img/landing-bg.webp"
-        alt="Imagen de gente pasandolo bien en un concierto" />
-</template>
