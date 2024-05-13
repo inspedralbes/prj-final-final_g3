1deee70486284dd8f980a94e4ad692f2dcc1b0e5
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-13 13:19:24 +0200

    refactor: Remove commented out code in register.vue

--- nuxt/pages/auth/callback/google.vue
+++ nuxt/pages/auth/callback/google.vue
@@ -69,7 +69,7 @@ export default {
                 }
             } catch (error) {
                 console.error(error);
-                this.$router.push('/join');
+                // this.$router.push('/join');
             }
         }
     }
