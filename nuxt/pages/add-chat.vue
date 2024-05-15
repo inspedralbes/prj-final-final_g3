21013fb1e658d0b252c26b7ca6461ffa16762e20
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-15 09:26:03 +0200

    chore: Refactor add-chat page to improve user experience and code readability

--- nuxt/pages/add-chat.vue
+++ nuxt/pages/add-chat.vue
@@ -12,14 +12,14 @@
 
         <!-- Lista de usuarios que devuelve el buscador -->
         <main class="flex justify-center items-center mt-8">
-            <div v-if="!this.empty" v-for="user in filteredUsers" :key="user.id" class="justify-center items-start gap-1 max-w-64">
+            <div v-if="!this.empty" v-for="user in filteredUsers" :key="user.id"
+                class="justify-center items-start gap-1 max-w-64">
                 <!-- <img class="size-16 rounded-full object-cover"
                     src="{{ user.avatar }}"
                     alt=""> -->
-                    <NuxtLink :to="'/chat'" :user="user">
-                        <h2 class="font-bold m-5">{{ user.nickname }}</h2>
-                    </NuxtLink>
-
+                <button @click="goToChat(user)">
+                    <h2 class="font-bold m-5">{{ user.nickname }}</h2>
+                </button>
             </div>
             <div v-if="this.empty" class="flex justify-center items-center gap-1">
                 <h2 class="font-bold m-5">{{ message }}</h2>
@@ -27,14 +27,14 @@
         </main>
 
         <router-link to="/chats">
-        <svg class="w-auto h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-          stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M5 12l14 0" />
-          <path d="M5 12l6 6" />
-          <path d="M5 12l6 -6" />
-        </svg>
-      </router-link>
+            <svg class="w-auto h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M5 12l14 0" />
+                <path d="M5 12l6 6" />
+                <path d="M5 12l6 -6" />
+            </svg>
+        </router-link>
     </div>
     <!-- <Menu /> -->
 </template>
@@ -56,16 +56,19 @@ export default {
         async searchUsers() {
             const response = await comManager.searchUsers(this.param);
 
-            if(response.data.length > 0) {
+            if (response.data.length > 0) {
                 this.empty = false;
-                this.users = response.data; 
+                this.users = response.data;
                 console.log(this.users);
-                this.filteredUsers = this.users.map(user => ({ id: user.id, nickname: user.nickname, avatar: user.avatar}));
+                this.filteredUsers = this.users.map(user => ({ id: user.id, nickname: user.nickname, avatar: user.avatar }));
             } else {
                 this.empty = true;
                 this.message = response.data.message;
             }
-        }
+        },
+        goToChat(user) {
+            this.$router.push({ name: 'chat', params: { id: user.id } });
+        },
     },
 }
 </script>
