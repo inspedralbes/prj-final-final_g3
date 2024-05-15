5e1400466693b6e27efc00de3444f4041efea636
a22ivbs <a22ivabarsan@inspedralbes.cat>
2024-05-15 09:18:44 +0200

    refactor: Add back button to add-chat page

--- nuxt/pages/add-chat.vue
+++ nuxt/pages/add-chat.vue
@@ -26,7 +26,15 @@
             </div>
         </main>
 
-
+        <router-link to="/chats">
+        <svg class="w-auto h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+          stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M5 12l14 0" />
+          <path d="M5 12l6 6" />
+          <path d="M5 12l6 -6" />
+        </svg>
+      </router-link>
     </div>
     <!-- <Menu /> -->
 </template>
