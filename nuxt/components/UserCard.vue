0dd862bd23bdb1ac1bda7d09cc56c533b6832796
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-23 12:43:34 +0200

    refactor: Remove unused code and update profile page layout

---
+++ nuxt/components/UserCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{ user }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped></style>
---
+++ nuxt/pages/perfil/followers.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Followers</h1>
+    </div>
+</template>
+
+<script>
+import { useStores } from '@/stores/counter.js';
+import comManager from '@/managers/comManager.js';
+
+export default {
+    data() {
+        return {
+            store: useStores(),
+            followers: [],
+        }
+    },
+    mounted() {
+        this.getFollowers();
+    },
+    methods: {
+        async getFollowers() {
+            comManager.getFollowers().then((followers) => {
+                this.followers = followers;
+            });
+        }
+    }
+}
+</script>
+
+<style scoped></style>
