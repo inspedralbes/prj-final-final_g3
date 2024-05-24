727e2b30e725eb6318869ade1ffd649483ef5963
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-24 08:22:17 +0200

    refactor: Remove followers and following pages

--- nuxt/pages/perfil/[type].vue
+++ nuxt/pages/perfil/[type].vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <h1>Followers</h1>
-        <div v-for="follower in followers" class="w-full flex flex-col justify-center items-center">
+        <h1>{{ type }}</h1>
+        <div v-if="type === 'followers'" v-for="follower in followers"
+            class="w-full flex flex-col justify-center items-center">
             <CardUser class="w-2/3" :user="follower.follower" />
+            <hr>
+        </div>
+        <div v-else v-for="followed in following.followed" class="w-full flex flex-col justify-center items-center">
+            <CardUser class="w-2/3" :user="followed.followed" />
             <hr>
         </div>
     </div>
@@ -19,12 +24,14 @@ export default {
             store: useStores(),
             followers: computed(() => this.store.userInfo.followersUsers.followers),
             following: computed(() => this.store.userInfo.followingUsers.followed),
+            type: this.$route.params.type,
         }
     },
     mounted() {
         if (!this.store.getLoggedIn()) return this.$router.push('/join');
         if (!this.followers) this.getFollowers();
         if (!this.following) this.getFollowing();
+        console.log("Dato de url:", this.type);
     },
     methods: {
         async getFollowers() {
