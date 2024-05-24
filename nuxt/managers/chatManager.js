2947122629deae09abaa76234e9b30387176ca49
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-24 10:45:46 +0200

    refactor: Update chatManager and userManager to use consistent variable name for API URL

--- nuxt/managers/chatManager.js
+++ nuxt/managers/chatManager.js
@@ -9,7 +9,7 @@ if (env.toLowerCase() === "development") {
   url_api = import.meta.env.VITE_APP_API_DEV_URL;
   url_api_mongo = import.meta.env.VITE_APP_MONGO_API_DEV_URL;
 } else if (env.toLowerCase() === "production") {
-  url = import.meta.env.VITE_APP_API_PROD_URL;
+  url_api = import.meta.env.VITE_APP_API_PROD_URL;
   url_api_mongo = import.meta.env.VITE_APP_API_PROD_MONGO_URL;
 }
 
@@ -40,8 +40,8 @@ async function getFirst10Messages(chat_id) {
   try {
     const response = await axios.get(`${url_api_mongo}/messages`, {
       params: {
-        chat_id: chat_id
-      }
+        chat_id: chat_id,
+      },
     });
     return response.data;
   } catch (error) {
@@ -50,26 +50,25 @@ async function getFirst10Messages(chat_id) {
 }
 
 async function getMessages(chat_id, message_id) {
-
-  try{
+  try {
     const response = await axios.get(`${url_api_mongo}/get10messages`, {
       params: {
         chat_id: chat_id,
-        message_id: message_id
-      }
+        message_id: message_id,
+      },
     });
     return response.data;
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
 }
 
 async function getChats(user_id) {
   try {
-    const response = await axios.get(`${url_api_mongo}/chats`,{
-      params: { 
-        user_id: user_id 
-      }
+    const response = await axios.get(`${url_api_mongo}/chats`, {
+      params: {
+        user_id: user_id,
+      },
     });
     return response.data;
   } catch (error) {
@@ -81,16 +80,16 @@ async function getUserChats(user_id) {
   const store = useStores();
 
   try {
-    const response = await axios.get(`${url_api}/getUser?user_id=${user_id}`,{
+    const response = await axios.get(`${url_api}/getUser?user_id=${user_id}`, {
       headers: {
         Authorization: `Bearer ${store.getToken()}`,
-      }
+      },
     });
     const obj_response = {
       nickname: response.data.nickname,
       avatar: response.data.avatar,
-    }
-    
+    };
+
     return obj_response;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -103,7 +102,7 @@ const comChat = {
   getFirst10Messages,
   getMessages,
   getChats,
-  getUserChats
+  getUserChats,
 };
 
 export default comChat;
