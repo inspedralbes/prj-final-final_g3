9eaaecfc95aec7603e3770460a8202579f1d60d2
a16margarlop <a16margarlop@inspedralbes.cat>
2024-05-25 13:18:00 +0200

    refactor: Add lazy loading for images in EventosProfile.vue and remove unused code in perfil.vue

--- nuxt/components/EventosProfile.vue
+++ nuxt/components/EventosProfile.vue
@@ -1,9 +1,16 @@
 <template>
-    <section class='flex justify-center text-white'>
-        <p v-if="!eventos">No tienes eventos disponibles.</p>
-        <article v-else>
-            
-        </article>
+    <p class="text-center" v-if="!events">No tienes eventos disponibles.</p>
+    <section v-else class='w-[90%] mx-auto flex flex-col gap-3'>
+        <NuxtLink :to="`/events/${event.id}`" class="relative w-full h-48 rounded-md overflow-hidden" v-for="event in events" :key="event.id">
+            <div class='relative'>
+                <img :src="JSON.parse(event.images)[2]" class='w-full h-full object-cover'
+                    :alt="`Foto de ${event.event}`" />
+                <div
+                    class='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-40% from-transparent to-[#212121]'>
+                </div>
+            </div>
+            <h1 class="absolute bottom-2 left-2 font-bold">{{ event.event }}</h1>
+        </NuxtLink>
     </section>
 </template>
 
@@ -14,14 +21,14 @@ export default {
     data() {
         return {
             store: useStores(),
-            eventos: [],
+            events: [],
         }
     },
     methods: {
         async getEventos() {
             const allEvents = this.store.getEvents()
-            this.eventos = allEvents.filter(event => event.like === true);
-            console.log(this.eventos)
+            this.events = allEvents.filter(event => event.like === true);
+            console.log(this.events)
         }
     },
 
