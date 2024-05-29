79bc26b30b43bb96730492f96727b122af920778
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-30 01:03:59 +0200

    Refactor perfil/[username]/[type].vue component for improved user followers and following display

--- nuxt/pages/perfil/[username]/[type].vue
+++ nuxt/pages/perfil/[username]/[type].vue
@@ -7,15 +7,14 @@
             </NuxtLink>
             <UTabs v-model="type" :items="[{ label: 'Seguidors' }, { label: 'Seguits' }]" class="mx-2 w-2/3"></UTabs>
         </div>
-        <div>
+        <div v-if="checkUser">
             <div v-if="type === 0 && followers.length != 0" v-for="follower in followers"
                 class="w-full flex flex-col justify-center items-center">
                 <CardUser class="w-2/3" :user="follower.follower" />
                 <hr>
             </div>
             <div v-else-if="type === 0 && followers.length === 0" class="flex justify-center items-center">
-                <h2 v-if="checkUser" class="font-bold">No tens seguidors</h2>
-                <h2 v-else class="font-bold">Aquest usuari no té seguidors</h2>
+                <h2 class="font-bold">No tens seguidors</h2>
             </div>
             <div v-else-if="type === 1 && following.length != 0" v-for="followed in following"
                 class="w-full flex flex-col justify-center items-center">
@@ -23,28 +22,27 @@
                 <hr>
             </div>
             <div v-else-if="type === 1 && following.length === 0" class="flex justify-center items-center">
-                <h2 v-if="checkUser" class="font-bold">No segueixes a ningú</h2>
-                <h2 v-else class="font-bold">Aquest usuari no segueix a ningú</h2>
+                <h2 class="font-bold">No segueixes a ningú</h2>
             </div>
         </div>
-        <!-- <div v-else>
-            <div v-if="type === 0 && oFollowers.length != 0" v-for="follower in oFollowers"
+        <div v-else>
+            <div v-if="type === 0 && otherFollowers.length != 0" v-for="follower in otherFollowers"
                 class="w-full flex flex-col justify-center items-center">
                 <CardUser class="w-2/3" :user="follower.follower" />
                 <hr>
             </div>
-            <div v-else-if="type === 0 && oFollowers.length === 0" class="flex justify-center items-center">
+            <div v-else-if="type === 0 && otherFollowers.length === 0" class="flex justify-center items-center">
                 <h2 class="font-bold">No té seguidors</h2>
             </div>
-            <div v-else-if="type === 1 && oFollowing.length != 0" v-for="followed in oFollowing"
+            <div v-else-if="type === 1 && otherFollowing.length != 0" v-for="followed in otherFollowing"
                 class="w-full flex flex-col justify-center items-center">
                 <CardUser class="w-2/3" :user="followed.followed" />
                 <hr>
             </div>
-            <div v-else-if="type === 1 && oFollowing.length === 0" class="flex justify-center items-center">
+            <div v-else-if="type === 1 && otherFollowing.length === 0" class="flex justify-center items-center">
                 <h2 class="font-bold">No segueix a ningú</h2>
             </div>
-        </div> -->
+        </div>
     </div>
     <Menu />
 </template>
@@ -58,10 +56,10 @@ export default {
     data() {
         return {
             store: useStores(),
-            // followers: computed(() => this.store.userInfo.followersUsers.followers),
-            // following: computed(() => this.store.userInfo.followingUsers.followed),
-            // oFollowing: computed(() => this.store.otherUserInfo.followingUsers.followed) || [],
-            // oFollowers: computed(() => this.store.otherUserInfo.followersUsers.followers) || [],
+            followers: computed(() => this.store.userInfo.followersUsers.followers),
+            following: computed(() => this.store.userInfo.followingUsers.followed),
+            otherFollowing: computed(() => this.store.otherUserInfo.followingUsers.followed) || [],
+            otherFollowers: computed(() => this.store.otherUserInfo.followersUsers.followers) || [],
             type: this.$route.params.type === 'followers' ? 0 : 1,
             user: this.$route.params.username
         }
@@ -94,20 +92,20 @@ export default {
         checkUser() {
             return this.store.userInfo.username === this.user
         },
-        followers() {
-            if (this.checkUser) {
-                return this.store.userInfo.followersUsers.followers
-            } else {
-                return this.store.otherUserInfo.followersUsers.followers
-            }
-        },
-        following() {
-            if (this.checkUser) {
-                return this.store.userInfo.followingUsers.followed
-            } else {
-                return this.store.otherUserInfo.followingUsers.followed
-            }
-        }
+        // followers() {
+        //     if (this.checkUser) {
+        //         return this.store.userInfo.followersUsers.followers
+        //     } else {
+        //         return this.store.otherUserInfo.followersUsers.followers
+        //     }
+        // },
+        // following() {
+        //     if (this.checkUser) {
+        //         return this.store.userInfo.followingUsers.followed
+        //     } else {
+        //         return this.store.otherUserInfo.followingUsers.followed
+        //     }
+        // }
     }
 }
 </script>
